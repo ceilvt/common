6dc2bf60d7f5a3ce6b66cbd1b079990aa88aa0da
a <30483275@qq.com>
2015-12-31 16:23:05 +0800

    向html中添加音乐播放按钮

---
+++ music/js/music.js
@@ -0,0 +1,28 @@
+//先将jquery文件引入
+//@musicDir mp3,play.png,pause.png目录
+//@musicName mp3名字
+function addMusic(musicDir,musicName){
+    var style='#music_wrap{width:30px;position:absolute;top:20px;right:20px;font-size:0}.musicRotate{animation:1s linear infinite linerotate;-webkit-animation:1s linear infinite linerotate}@keyframes linerotate{0%{transform:rotate(0deg)}100%{transform:rotate(360deg)}}@-webkit-keyframes linerotate{0%{-webkit-transform:rotate(0deg)}100%{-webkit-transform:rotate(360deg)}}#music_wrap img{width:100%;display:block}';
+    var music_style = $('<style></style>');
+        music_style.attr({'type':'text/css'});
+        music_style.html(style);
+    var music_div = $('<div id="music_wrap"><img src="'+musicDir+'play.png"'+'></div>');
+        music_div.addClass('musicRotate');
+    var audio = $('<audio id="music"></audio>');
+        audio.attr({'src':musicDir+'/'+musicName+'.mp3','autoplay':'true','loop':true});
+    $('body').append(audio);
+    $('body').append(music_div);
+    $('head').append(music_style);
+    $("body").delegate("#music_wrap","touchstart",function(){
+        //$(this).toggleClass('musicRotate');
+        if($('#music')[0].paused){
+            $(this).addClass('musicRotate');
+            $('#music_wrap img').attr({'src':musicDir+'play.png'});
+            $('#music')[0].play();
+        }else{
+            $(this).removeClass('musicRotate');
+            $('#music_wrap img').attr({'src':musicDir+'pause.png'});
+            $('#music')[0].pause();
+        };
+    });
+}
